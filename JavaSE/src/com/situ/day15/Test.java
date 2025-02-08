c54a2b41f05f231088de2db938eb8f7cb68f48bf
lijiankun <1732931774@qq.com>
2025-02-08 16:58:10 +0800

    day15(js)

---
+++ JavaSE/src/com/situ/day15/Test.java
@@ -0,0 +1,19 @@
+package com.situ.day15;
+
+/**
+ * @author OfferKing
+ * @version 1.0
+ * @date 2025/2/8 15:51
+ */
+public class Test {
+    public static void main(String[] args) {
+        Integer a = 100;
+        Integer b = 100;
+        System.out.println(a == b);  // 输出 true
+
+        Integer x = 200;
+        Integer y = 200;
+        System.out.println(x == y);  // 输出 false
+
+    }
+}
